8544ad4f127c09f47985d5db37071cdd6acf2b6f
Nicholas Dechert <dechert.nicholas@gmail.com>
2021-06-09 12:25:24 -0700

    Add project files.

---
+++ SQLiteDBHandler/Model.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace SQLiteDBHandler
+{
+    public class SQLiteContext : DbContext
+    {
+        public DbSet<Type> Types { get; set; }
+        public DbSet<Group> Groups { get; set; }
+        public DbSet<Blueprint> Blueprints { get; set; }
+        public DbSet<Graphic> Graphics { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite(@"Data Source=Industry.db");
+    }
+
+    public class Type
+    {
+        public int TypeID { get; set; }
+        public int graphicID { get; set; }
+        public Dictionary<string, string> typeName { get; set; }
+        public int groupID { get; set; }
+    }
+
+    public class Group
+    {
+        public int GroupId { get; set; }
+    }
+
+    public class Blueprint
+    {
+        public int BlueprintId { get; set; }
+    }
+
+    public class Graphic
+    {
+        public int GraphicId { get; set; }
+    }
+}
